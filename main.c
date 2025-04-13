1b387a0e64d6de252ac0540ff43569253aa00f00
Gabigo401 <gabrielchoque017@gmail.com>
2025-04-13 20:25:06 -0300

    Criada 2 variaveis que recebem os valores digitados o primeiro scanf le e armazena no endereço da variavel chamada valor o numero digitado e o segundo scanf le e armazena a senha digitada no endereço da variavel senha

--- main.c
+++ main.c
@@ -24,8 +24,15 @@ void DepositoCliente(){
 
 }
 
-void SaqueCliente(){
-
+void SaqueCliente1(Cliente1 *cliente){   //a função acessa a struct cliente1 como um ponteiro
+    float valor;  //variavel float chamado valor que recebe o valor digitado
+	__int64 senhaInput;  //variavel int que recebe a senha digitada
+	printf("Digite o valor do saque desejado:\n");
+	scanf("%f", &valor);  //scanf que le e armazena na variavel de valor o valor digitado
+	
+	printf("Digite a senha para validar o saque:\n");
+	scanf("%lld", &senhaInput); //scanf que le e armazena na variavel de senha a senha digitada
+	
 }
 
 void CompraCriptoCliente(){
