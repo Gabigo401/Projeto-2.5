0868fa5265124635b271feb5a9efe123e9d0cf69
Gabigo401 <gabrielchoque017@gmail.com>
2025-04-13 20:20:07 -0300

    Aqui foi adicionado uma função de deposito para o cliente2, ela e igual a outra so mudando a struct que ela acessa

--- main.c
+++ main.c
@@ -41,6 +41,27 @@ void DepositoCliente1(Cliente1 *cliente){  //ao fazer a função dentro do paren
 	}
 }
 
+void DepositoCliente2(Cliente2 *cliente){  
+    float valor;    
+	__int64 senhaInput;  
+	printf("Digite o valor do deposito desejado:\n");
+	scanf("%f", &valor);  
+
+    printf("Digite a senha para validar o deposito:\n");
+	scanf("%lld", &senhaInput);  
+
+    if(senhaInput == cliente ->senha){ 
+		if( valor > 0){   
+		cliente -> reais += valor; 
+		printf("Deposito realizado com exito!. Saldo Atual: R$ %.2f\n", cliente -> reais); 
+	}else { 
+		printf("Valor invalido para fazer o deposito.\n");
+	}
+	}else {   
+		printf("Senha incorreta, Tente novamente.\n");
+	}
+}
+
 void SaqueCliente(){
 
 }
