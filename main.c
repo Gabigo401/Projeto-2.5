721958fbd44e4a062cccfae1029b6ef9ad251e25
Gabigo401 <gabrielchoque017@gmail.com>
2025-04-13 17:46:24 -0300

    adicionado as funções que usarei no futuro

---
+++ main.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+
+void ExibirMenu(){
+
+}
+
+void Carteira(){
+
+}
+
+void RegistrarTransacao(){
+
+}
+
+void ExibirExtrato(){
+
+}
+
+void DepositoCliente(){
+
+}
+
+void SaqueCliente(){
+
+}
+
+void CompraCriptoCliente(){
+
+}
+
+void VendaCriptoCliente(){
+
+}
+
+void atl_cot_cripto(){
+
+}
+
+void mostrar_cots(){
+
+}
+
+void escreveArquivo(){
+
+}
+
+void leArquivo(){
+
+}
+
+
+int main(){
+    return 0;
+}
