fa7336f18665a93b65142a6113a5c9d85b674bee
Gabigo401 <gabrielchoque017@gmail.com>
2025-04-13 20:31:44 -0300

    adicionado 2 if que verifica a senha se e igual ao da struct que foi acessada no inicio da função e outra que verifica se o valor e maior que zero e se o valor e menor que os valores que ja estão guardados na struct do cliente se passa pelos 2 if o programa atualiza a struct do cliente subtraindo os valores que estão guardados nela com os valores digitados pelo usuario e atualiza a struct com esses novos valores

--- main.c
+++ main.c
@@ -33,6 +33,16 @@ void SaqueCliente1(Cliente1 *cliente){   //a função acessa a struct cliente1 c
 	printf("Digite a senha para validar o saque:\n");
 	scanf("%lld", &senhaInput); //scanf que le e armazena na variavel de senha a senha digitada
 	
+    if(senhaInput == cliente ->senha){   //if que verifica se  senha recebida e igual ao da struct de cliente1
+        if(valor > 0 && valor <= cliente ->reais){  //if que verifica se o valor e maior que zero e verifica se e  menor que o valor que ja esta armazenado na struct de cliente1, essa comparação e feita usando alguns ponteiros para acessar os valores de 'reais'
+		cliente ->reais -= valor;  //acessa os valores de 'reais' da struct do cliente e faz  o subtração do valor dela com o valor digitado pelo usuario e atualiza ela com os novos valores
+		printf("Saque feito com sucesso!. Saldo atual R$ %.2f\n", cliente -> reais);
+	}else {  //else que mostra se o valor e invalido ou se o valor digitado e maior que o valor guardado na struct cliente1
+		printf("Saldo insuficiente ou valor invalido.");
+	}
+	 }else {  //else que avisa se a senha estiver e errada
+		printf("Senha incorreta, operaï¿½ï¿½o cancelada.\n");
+	}
 }
 
 void CompraCriptoCliente(){
