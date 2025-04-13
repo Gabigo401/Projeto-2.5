a84b0bac78f19c29e8c6f0cec831d73ae3898648
Gabigo401 <gabrielchoque017@gmail.com>
2025-04-13 19:35:06 -0300

    Adicionei alguns printf mostrando uma mensagem de menu e mostrando 8 opções que vai de 0 ate o 7

--- main.c
+++ main.c
@@ -5,7 +5,15 @@
 
 
 void ExibirMenu(){
-
+    printf("Menu: \n");
+    printf("1- Carteira do Investidor\n");
+    printf("2- Extratos\n");
+    printf("3- Deposito\n");
+    printf("4- Saque\n");
+    printf("5- Comprar criptomoedas\n");
+    printf("6- Vender criptomoedas\n");
+    printf("7- Atualizar cotaï¿½ï¿½es\n");
+    printf("0 - Sair\n");
 }
 
 void Carteira(){
