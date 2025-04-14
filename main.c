cd27f8ca4aec1016c05514a4174e7c4378f7cb1f
Gabigo401 <gabrielchoque017@gmail.com>
2025-04-14 01:19:40 -0300

    Chamando a função para registrar as transações nela e chamando as structs cripto e transacoes

--- main.c
+++ main.c
@@ -32,7 +32,7 @@ void CompraCriptoCliente(){
 
 }
 
-void VendaCriptoCliente1(Cliente1 *cliente){
+void VendaCriptoCliente1(Cliente1 *cliente, Cripto criptos[], transacao historico[], int *total){  //chamando a struct de criptos e a de transacao
 	char confirmacao;   //char de que vai ser usado para receber a confirmação do cliente
 	float tax_venda_bit = 0.03;   //float mostrando a taxa de venda do bitcoin
 	float tax_venda_ethe = 0.02;  //float mostrando a taxa de venda do etherium
@@ -68,6 +68,7 @@ void VendaCriptoCliente1(Cliente1 *cliente){
 					valor_recebido = quantos * criptos[0].cotacao;   //guarda na variavel valor_recebido a quantidade de moedas vendidas vezes a cotação do bitcoin
 					cliente ->bitcoin -= quantos;   //acessa a struct do cliente e diminui os valores de bitcoin que estão armazenados e diminui pela quantidade que foi digitada para ser vendida
 					cliente ->reais += quantos * criptos[0].cotacao; //acessa o valor de reais que esta na struct do cliente e soma com a  quantidade de moedas que foi escolhida para venda que foi multiplicada pelo valor da cotação da moeda
+				    RegistrarTransacao(historico, total, "Venda", "Bitcoin", quantos); //chamada da função para salvar as operações
 				}else printf("Quantidade desejada e insuficiente.\n");  //mensagem se o valor escolhido para venda for maior que o valor ns struct do cliente
 				break;
 			case 2:
@@ -77,7 +78,7 @@ void VendaCriptoCliente1(Cliente1 *cliente){
 					valor_recebido = quantos * criptos[1].cotacao;  //a variavel velorrecebido recebe o valor da variavel quantos que foi multiplicada pela cotação do etherium
 					cliente ->etherium -= quantos;   //acessa os valores de etherium do cliente e  subtrai ela com o valor que foi escolhido para venda
 					cliente ->reais += quantos * criptos[1].cotacao; //acessa o valor de reais do cliente se soma com o valor que foi recebido pela venda que seria a quantidade escolhida para venda multiplicado pela cotação da moeda
-					
+					RegistrarTransacao(historico, total, "Venda", "Bitcoin", quantos);  //chamando para salvar as operações
 				}else printf("Quantidade desejada e insuficiente.\n");
 				break;
 			case 3:
@@ -87,7 +88,7 @@ void VendaCriptoCliente1(Cliente1 *cliente){
 					valor_recebido = quantos * criptos[2].cotacao;   //a variavel valor_recebido recebe o valor resultante da multiplicação entre a variavel quantos e a cotação da moeda escolhida
 					cliente ->ripple -= quantos;  //acessa os valores de ripple do cliente e subtrai ela com o valor que foi escolhido para venda
 					cliente ->reais += quantos * criptos[2].cotacao;   //acessa os valores de reais do cliente e soma com os valores resultante da multiplicação entre a variavel quanto e a cotação da moeda escolhida
-					
+					RegistrarTransacao(historico, total, "Venda", "Bitcoin", quantos);  //chamando para salvar as operações
 				}else printf("Quantidade desejada e insuficiente.\n");  //else se a quantidade escolhida para venda e maior que a armazenada na struct
 				break;
 			default:
@@ -116,7 +117,7 @@ void VendaCriptoCliente1(Cliente1 *cliente){
 	}
 }
 
-void VendaCriptoCliente2(Cliente2 *cliente){
+void VendaCriptoCliente2(Cliente2 *cliente, Cripto criptos[], transacao historico[], int *total){
 	char confirmacao;   
 	float tax_venda_bit = 0.03;   
 	float tax_venda_ethe = 0.02;  
@@ -152,6 +153,7 @@ void VendaCriptoCliente2(Cliente2 *cliente){
 					valor_recebido = quantos * criptos[0].cotacao;  
 					cliente ->bitcoin -= quantos;   
 					cliente ->reais += quantos * criptos[0].cotacao; 
+				    RegistrarTransacao(historico, total, "Venda", "Bitcoin", quantos);
 				}else printf("Quantidade desejada e insuficiente.\n"); 
 				break;
 			case 2:
@@ -161,7 +163,7 @@ void VendaCriptoCliente2(Cliente2 *cliente){
 					valor_recebido = quantos * criptos[1].cotacao; 
 					cliente ->etherium -= quantos;  
 					cliente ->reais += quantos * criptos[1].cotacao; 
-					
+					RegistrarTransacao(historico, total, "Venda", "Bitcoin", quantos);
 				}else printf("Quantidade desejada e insuficiente.\n");
 				break;
 			case 3:
@@ -171,7 +173,7 @@ void VendaCriptoCliente2(Cliente2 *cliente){
 					valor_recebido = quantos * criptos[2].cotacao;   
 					cliente ->ripple -= quantos;  
 					cliente ->reais += quantos * criptos[2].cotacao;   
-					
+					RegistrarTransacao(historico, total, "Venda", "Bitcoin", quantos);
 				}else printf("Quantidade desejada e insuficiente.\n"); 
 				break;
 			default:
