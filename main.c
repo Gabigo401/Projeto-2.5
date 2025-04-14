14bb0130dc857ca206645a345e0fbdbe1684a200
Gabigo401 <gabrielchoque017@gmail.com>
2025-04-14 01:35:43 -0300

    Adicionando nos case as  chamadas da função de cada opção e escrevendo no arquivo txt os valores da carteira  e do extrato

--- main.c
+++ main.c
@@ -3,8 +3,8 @@
 #include <string.h>
 #include <time.h>
 
-typedef struct {
-    char nome[100];
+typedef struct {   //struct pro cliente1
+    char nome[100]; 
     __int64 cpf;
     __int64 senha;
     float reais;
@@ -13,7 +13,7 @@ typedef struct {
     float ripple;
 }Cliente1;
 
-typedef struct{
+typedef struct{  //struct pro cliente2
     char nome[100];
     __int64 cpf;
     __int64 senha;
@@ -73,27 +73,27 @@ void leArquivo(){
 
 
 int main(){
-    Cliente1 cliente1 = {"Luanda Soliz", 34567895734, 2344568, 500.00, 20.00, 100.00, 648.25};
-    Cliente2 cliente2 = {"Lidia Mamani", 23498745690, 2356235, 300.00, 26.00, 487.00, 654.00};
+    Cliente1 cliente1 = {"Luanda Soliz", 34567895734, 2344568, 500.00, 20.00, 100.00, 648.25};  //adicionando informações no cliente1
+    Cliente2 cliente2 = {"Lidia Mamani", 23498745690, 2356235, 300.00, 26.00, 487.00, 654.00};  //e no cliente2
 
-    __int64 cpfInput, senhaInput;
-    int logado = 0;
-    int ClienteAtual =0;
+    __int64 cpfInput, senhaInput;   //criando duas variaveis para senha e cpf
+    int logado = 0;   //int chamada logad
+    int ClienteAtual =0;  //int para saber qual cliente acessou
     printf("Bem vindo! Faca o login para continuar.\n");
     printf("Digite o CPF:\n");
-    scanf("%lld", &cpfInput);
+    scanf("%lld", &cpfInput);  //scanf para pegar o cpf digitado e guardar no endereço
     printf("Digite a senha:\n");
-    scanf("%lld",  &senhaInput);
+    scanf("%lld",  &senhaInput);  //scanf para pegar a senha e guardar
 
-    if(cpfInput == cliente1.cpf && senhaInput == cliente1.senha){
+    if(cpfInput == cliente1.cpf && senhaInput == cliente1.senha){  //if para comprar o cpf digitado com o cpf do cliente e comparando a senha tambem
     	printf("\nLogin foi feito com sucesso!, %s.\n", cliente1.nome);
-		logado = 1;
-		ClienteAtual = 1;
+		logado = 1;  //se o login funcionoi a variavel logado vira 1 para mostrar que alguem esta logado
+		ClienteAtual = 1;  //a variavel clienteatual muda para 1 para mostrar que e o cliente1
 	}
-	else if(cpfInput == cliente2.cpf && senhaInput == cliente2.senha){
+	else if(cpfInput == cliente2.cpf && senhaInput == cliente2.senha){  //comparando se condiz com o cpf ou senha do outro cliente
 		printf("\nLogin foi feito com sucesso!, %s.\n", cliente2.nome);
-		logado = 1;
-		ClienteAtual = 2;
+		logado = 1;  //muda para 1 se alguem logou
+		ClienteAtual = 2;  //muda para 2 mostrando que e o cliente2 logado
 	}
     
     if (logado){
@@ -102,39 +102,52 @@ int main(){
 			ExibirMenu();
 			scanf("%d", &opcao);
 			
-			switch(opcao){
+			switch(opcao){  //switch case para as opções mostradas no menu
 				case 1:
 					if(ClienteAtual == 1){
+						Carteira1(cliente1);  //chamando a função de mostrar carteira do cliente1
+					   escreveArquivo("cliente1.txt", &cliente1, criptos, 3, historico_transacoes1, total_transacoes1);  //chamando a função de escreverarquivo para escrever  tudo em um txt
 					}else{
+						Carteira2(cliente2);  //se for o cliente2 esse e chamado
+					   escreveArquivo2("cliente2.txt", &cliente2);  //salvando em outro txt o do cliente2
 				}
 					break;
 				case 2:
 					if(ClienteAtual == 1){
+						ExibirExtrato(historico_transacoes1, total_transacoes1);  //exibindo o extrato do cliente1
+					   escreveArquivo("cliente1.txt", &cliente1, criptos, 3, historico_transacoes1, total_transacoes1);  //escrevendo no txt o extrato
 					}else{
 				}
 					break;
 				case 3:
 					if(ClienteAtual == 1){
+						DepositoCliente1(&cliente1, historico_transacoes1, &total_transacoes1);  //chamando a função de deposito do cliente1
 					}else{
 				}
 					break;
 				case 4:
 					if(ClienteAtual == 1){
+						SaqueCliente1(&cliente1, historico_transacoes1, &total_transacoes1);  //chamando a função para o saque do cliente1
 					}else{
 				}
 					break;
 				case 5:
 					if(ClienteAtual ==1){
+						CompraCriptoCliente1(&cliente1, criptos, historico_transacoes1, &total_transacoes1);  //chamando a função para comprar as criptomoedas
 					}else{
 				}
 					break;
 				case 6:
 					if(ClienteAtual == 1){
+						VendaCriptoCliente1(&cliente1, criptos, historico_transacoes1, &total_transacoes1);  //chamando a funções de vender as criptomoedas
 					}else{
 				}
 					break;
 				case 7:
 					{
+						atl_cot_cripto(criptos, 3);  //chamando a função de atualizar as cotações
+					    mostrar_cots(criptos, 3);   //mostrando as cotações atualizados
+					    escreveArquivo("cliente1.txt", &cliente1, criptos, 3, historico_transacoes1, total_transacoes1);  //escrevendo as cotações no txt
 				}
 					break;
 				case 0:
