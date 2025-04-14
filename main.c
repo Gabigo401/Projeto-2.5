eb1a3f14519e989eecaaefa2353fa14b8605e308
Gabigo401 <gabrielchoque017@gmail.com>
2025-04-13 23:22:32 -0300

    switch case para o valor recebido pela variavel 'escolher' e dentro de cada opção disponivel a instruções que o programa executa, dentro de cada case a uma verificação se o valor escolhido para vendas e menor que o valor que o cliente tem na conta, apos isso, faz contar de quanto o cliente vai receber com a venda fazendo a multiplicação entre a quantidade escolhida para venda e a cotação da moeda escolhida e a quantidade de moedas vendidas apos a taxação, depois disso o programa acessa atravez de ponteiro os valores da moeda escolhida e subtrai com a quantidade que foi escolhida para venda e acessa o  valor de reais e soma com a quantidade que foi com a venda assim atualizando os valores das duas na struct do cliente, apos essas etapas a alguns else nos ifs que mostra uma mensagem se a quantidade escolhida para venda e maior que a quantidade que o cliente tem ou se a moeda escolhida pelo numero não esta entre as tres opções apresentadas

--- main.c
+++ main.c
@@ -60,6 +60,39 @@ void VendaCriptoCliente1(Cliente1 *cliente){
             printf("Compra cancelada.\n");
             return;
         }
+		switch(escolher) {  //switch case para cada uma das 3 opções possiveis
+			case 1:
+				if(quantos <= cliente ->bitcoin){  //verifica se a  quantidade de moedas desejada para venda e menor que a quantidade que esta armazena na struct do cliente
+					quantos_iniciais = quantos;    //faz a variavel quantos_iniciais pegar o mesmo valor da variavel quantos
+					quantos = quantos-(quantos * tax_venda_bit);    //a variavel quantos e diminuida fazendo uma subtração do valor vezes a taxa de venda do bitcoin
+					valor_recebido = quantos * criptos[0].cotacao;   //guarda na variavel valor_recebido a quantidade de moedas vendidas vezes a cotação do bitcoin
+					cliente ->bitcoin -= quantos;   //acessa a struct do cliente e diminui os valores de bitcoin que estão armazenados e diminui pela quantidade que foi digitada para ser vendida
+					cliente ->reais += quantos * criptos[0].cotacao; //acessa o valor de reais que esta na struct do cliente e soma com a  quantidade de moedas que foi escolhida para venda que foi multiplicada pelo valor da cotação da moeda
+				}else printf("Quantidade desejada e insuficiente.\n");  //mensagem se o valor escolhido para venda for maior que o valor ns struct do cliente
+				break;
+			case 2:
+				if(quantos <= cliente ->etherium){   //verificação de a quantidade escolhida para venda e menor que a quantidade que esta na struct do cliente
+					quantos_iniciais = quantos;  //a variavel quantosiniciais fica com o mesmo valor de quanto
+					quantos = quantos-(quantos * tax_venda_ethe);  //a variavel quantos e subtraida com o valor escolhido para venda multiplicada pela taxa de venda da moeda
+					valor_recebido = quantos * criptos[1].cotacao;  //a variavel velorrecebido recebe o valor da variavel quantos que foi multiplicada pela cotação do etherium
+					cliente ->etherium -= quantos;   //acessa os valores de etherium do cliente e  subtrai ela com o valor que foi escolhido para venda
+					cliente ->reais += quantos * criptos[1].cotacao; //acessa o valor de reais do cliente se soma com o valor que foi recebido pela venda que seria a quantidade escolhida para venda multiplicado pela cotação da moeda
+					
+				}else printf("Quantidade desejada e insuficiente.\n");
+				break;
+			case 3:
+				if(quantos <= cliente ->ripple){  //veriffica se a quantidade escolhida para venda e menor que a quantidade que tem no struct do cliente
+					quantos_iniciais = quantos;  //as duas variaveis ficam com os valores iguais
+					quantos = quantos-(quantos * tax_venda_rpl);   //a variavel  quantos e subtraida com o valor escolhida para venda multiplicada pela taxa de venda da moeda
+					valor_recebido = quantos * criptos[2].cotacao;   //a variavel valor_recebido recebe o valor resultante da multiplicação entre a variavel quantos e a cotação da moeda escolhida
+					cliente ->ripple -= quantos;  //acessa os valores de ripple do cliente e subtrai ela com o valor que foi escolhido para venda
+					cliente ->reais += quantos * criptos[2].cotacao;   //acessa os valores de reais do cliente e soma com os valores resultante da multiplicação entre a variavel quanto e a cotação da moeda escolhida
+					
+				}else printf("Quantidade desejada e insuficiente.\n");  //else se a quantidade escolhida para venda e maior que a armazenada na struct
+				break;
+			default:
+				printf("Criptomoeda invalida.\n"); //aparece isso se a moeda escolhida não esta cadastrada
+		}
 }
 
 void atl_cot_cripto(){
