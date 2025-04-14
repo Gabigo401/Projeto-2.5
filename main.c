1e037c23e2744be5becd172163a916fa00c25f2e
Gabigo401 <gabrielchoque017@gmail.com>
2025-04-14 00:55:31 -0300

    Criando outra struct para as transações e dando  valores para ela apos o main com valor maximo de 100 e criando ints para o total das transações começando por 0 e sera incrementada ao decorrer do codigo sempre que houver uma nova transação

--- main.c
+++ main.c
@@ -3,6 +3,11 @@
 #include <string.h>
 #include <time.h>
 
+typedef struct { //struct para guardar algumas funções ddas transações realizadas
+	char tipo[10];
+	char moeda[10];
+	float quantidade;
+}transacao;
 
 void ExibirMenu(){
 
@@ -12,8 +17,13 @@ void Carteira(){
 
 }
 
-void RegistrarTransacao(){
-
+void RegistrarTransacao(transacao historico[], int *total, const char *tipo, const char *moeda, float quantidade){ //chamando a struct transacao e criando variaveis para os parametros ja criados na struct
+	if (*total < 100) { //se  a variavel total for menor que 100 
+        strcpy(historico[*total].tipo, tipo);  //usando strcpy para copiar os valores da string para essa variavel
+        strcpy(historico[*total].moeda, moeda); //strcpy para copiar os valores da string nessa variavel
+        historico[*total].quantidade = quantidade; //pegando a variavel historico e salvando nela as transações ja feitar
+        (*total)++;  //incrementando +1 para nunca salva na mesma linha 
+    }
 }
 
 void ExibirExtrato(){
@@ -54,5 +64,12 @@ void leArquivo(){
 
 
 int main(){
+    transacao historico_transacoes1[100];  //pegando a struct transacao e adicionando uma variavei int para os total e colocando um limite para o historico
+    int total_transacoes1 = 0;
+
+    transacao historico_transacoes2[100];  //para o cliente 2
+    int total_transacoes2 = 0;
+
+
     return 0;
 }
