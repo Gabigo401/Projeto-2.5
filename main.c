9aa028fefb7b922c8c2fdcdb38a036e61beeb139
Gabigo401 <gabrielchoque017@gmail.com>
2025-04-15 22:32:49 -0300

    Na ffunção eu chamo como ponteiro um char constante e a struct do cliente1, alem disso, faço um vetor para a struct cripto e um int para ler as criptomoedas, apos isso eu crio um vetor para a struct transacao e um int para pegar o total das transações a seguir ocorre a criação de um ponteiro e abro ele com fopen dando um nome para o arquivo e a ação 'w' para escrever no arquivo, depois tem um if para verificar se o arquivo foi aberto

--- main.c
+++ main.c
@@ -44,8 +44,12 @@ void mostrar_cots(){
 
 }
 
-void escreveArquivo(){
-
+void escreveArquivo(const char *nome_arquivo, Cliente1 *cliente, Cripto criptos[], int qtdCriptos, transacao historico[], int totalTransacoes){ //crio um ponteiro com uma string constante e um ponteiro para a struct cliente1, um vetor da struct cripto e uma função int para percorrer o vetor sem sair do limite e um vetor pra struct transação junto com um int para o maximo de transações
+    FILE *arquivo = fopen(nome_arquivo, "w"); //criei um ponteiro para o arquivo e abro ele com o fopen e dentro dela dou um nome e a ação desejada, a letra 'W' significa write que seria que eu vou abrir o arquivo e escrever nele
+    if (arquivo == NULL) { //um if para confirmar se o arquivo foi aberto
+        perror("Erro ao abrir o arquivo");  //mensagem se ocorre um erro na abertura
+        return;
+    }
 }
 
 void leArquivo(){
