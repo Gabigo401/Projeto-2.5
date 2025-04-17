5187c826e7c3ab44f4ad1f532ad9b285d3dc3661
Gabigo401 <gabrielchoque017@gmail.com>
2025-04-16 22:12:00 -0300

    Adicionando umas chamadas no inicio da função como acessar a struct de cliente1 e transacao, alem disso, adicionar um char constante como ponteiro e um vetor para ler a struct de transacao, apos isso, ocorre a abertura do arquivo como ponteiro e utilizando a ação de ler o arquivo 'r' e usando um if para verificar se o arquivo foi aberto corretamente

--- main.c
+++ main.c
@@ -48,8 +48,12 @@ void escreveArquivo(){
 
 }
 
-void leArquivo(){
-
+void leArquivo(const char *nome_arquivo, Cliente1 *cliente, transacao historico_transacoes1[], int *total_Transacoes1){  //usando ponteiro num char constante e acessando a struct do cliente1 e de transacao usando um vetor, alem disso, criando um int com ponteiro
+	FILE *arquivo = fopen(nome_arquivo, "r");  //aqui o arquivo como ponteiro usando o fopen e o nome dele e uso a ação de ler 'r'
+  if (arquivo == NULL) {  //um if para verificar se o arquivo foi aberto
+    printf("Erro ao abrir o arquivo %s!\n", nome_arquivo);
+    return;
+  }
 }
 
 
